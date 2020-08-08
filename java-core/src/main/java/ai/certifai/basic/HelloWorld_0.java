6e16b52b0e47c2e8a1624aeb017d97e6aefa2f12
codenamewei <codenamewei@gmail.com>
2020-08-08 09:58:33 +0800

    wip

--- java-core/src/main/java/ai/certifai/basic/HelloWorld_0.java
+++ java-core/src/main/java/ai/certifai/basic/HelloWorld_0.java
@@ -2,13 +2,16 @@ package ai.certifai.basic;
 
 import ai.certifai.util.Evaluation;
 
+/**
+ * Change parameter of firstString (line 14) to "Hello World"
+ */
 public class HelloWorld_0
 {
     static Evaluation eval = new Evaluation(HelloWorld_0.class);
 
     public static void main(String[] args)
     {
-        String firstString = "Hello World";
+        String firstString = "";
 
         eval.evaluatePerLine(firstString);
     }
