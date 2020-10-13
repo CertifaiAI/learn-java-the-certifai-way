d615338ad573f5552ae8bfee9a73b293af34eea8
codenamewei <codenamewei@gmail.com>
2020-10-13 08:28:41 +0800

    wip

--- java-core/src/main/java/ai/certifai/maven/RunMaven.java
+++ java-core/src/main/java/ai/certifai/maven/RunMaven.java
@@ -15,6 +15,8 @@
  */
 package ai.certifai.maven;
 
+import ai.certifai.util.PathHandler;
+
 /**
  * Maven
  *
@@ -23,6 +25,46 @@ package ai.certifai.maven;
 public class RunMaven {
     public static void main(String[]args)
     {
+        /*
+        //TODO: Change this into your own image path
+        String sourceImagePath = "C:\\Users\\chiaw\\Desktop\\images\\chip\\70.jpg";
 
+        //Check image validity
+        if(PathHandler.isFileValid(sourceImagePath) == false)
+        {
+            System.out.println("Program abort. ");
+            return;
+        }
+
+        //Read in image
+        Mat opencvMat = imread(sourceImagePath);
+        int w = opencvMat.cols();
+        int h = opencvMat.rows();
+        int depth = opencvMat.depth();
+
+        if(depth == 1)
+        {
+            System.out.println("Image already is grayscale. Program abort");
+            return;
+        }
+
+        //Create output Mat to store grayscale image
+        Mat outputImage = new Mat();
+
+        //Convert the image to grayscale
+        cvtColor(opencvMat, outputImage, COLOR_RGB2GRAY);
+
+        //Save output image as file
+        String outputPath = PathHandler.getPathToFile(sourceImagePath) + "_grayscale.png";
+        imwrite(outputPath, outputImage);
+        System.out.println("The image is successfully to Grayscale");
+
+
+        imshow("Color Image", opencvMat);
+        imshow("Grayscale Image", outputImage);
+
+        //Press "Esc" to close window
+        if (waitKey(0) == 27) destroyAllWindows();
+        */
     }
 }
---
+++ java-core/src/main/java/ai/certifai/util/PathHandler.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020 CertifAI
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package ai.certifai.util;
+
+import java.io.File;
+
+public class PathHandler {
+
+    public static String getPathToFile(String pathName) {
+        String[] subString = pathName.split("/");
+        String fullPathName = subString[subString.length - 1];
+
+        String[] separator = fullPathName.split("\\.");
+
+        int fileEndIndex = fullPathName.length() - separator[(separator.length - 1)].length() - 1;
+        String fileName = fullPathName.substring(0, fileEndIndex);
+
+        Integer pathLength = pathName.length() - fullPathName.length();
+        String pathToSave = pathName.substring(0, pathLength);
+
+        fileName = fileName.replace(".", "_"); //replace any possible "." with "_"
+        fileName = fileName.replace(" ", ""); //replace any possible " " with ""
+
+        String pathFirstHalf = pathToSave + fileName;
+
+        return pathFirstHalf;
+    }
+
+    public static Boolean isFileValid(String pathName) {
+
+        try {
+            File fileImagePath = new File(pathName);
+
+            if (fileImagePath.exists() == false) {
+                System.out.println("File did not exist");
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error happened when reading in image.");
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
