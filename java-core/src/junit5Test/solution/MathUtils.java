53c59776ea10cf836c7802670635cd9171d56e29
Evan Yeoh Boon Khai <evanyeohboonkhai@yahoo.com>
2020-11-27 17:09:49 +0800

    add on junit5 demo and modify in pom dependency plugin

---
+++ java-core/src/junit5Test/solution/MathUtils.java
@@ -0,0 +1,34 @@
+package solution;
+
+/**
+ * Demo of Junit5 Test (Class)
+ *
+ * @author Boon Khai Yeoh
+ */
+
+public class MathUtils {
+
+    public int add(int a, int b){
+        return a+b;
+
+    }
+
+    public int minus(int a, int b){
+        return  a-b;
+
+    }
+
+    public int mul(int a, int b){
+        return  a*b;
+
+    }
+
+    public int divide(int a,int b){
+        return a/b;
+
+    }
+
+}
+
+
+
---
+++ java-core/src/junit5Test/solution/MathUtilsTest.java
@@ -0,0 +1,123 @@
+package solution;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Demo of Junit5 Test (Test File)
+ *
+ * @author Boon Khai Yeoh
+ */
+
+class MathUtilsTest {
+
+    /**
+     * Test Method 1 : Using assertEquals to compare two value.
+     *
+     * Usage :
+     * If both value are equal , the test will pass
+     *
+     */
+
+    @Test
+    @DisplayName("Add Function Test")
+    void add() {
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected,actual,"The sum expected to be 2");
+    }
+
+    /**
+     * Test Method 2 : Using assertAll to compare more value
+     *
+     * Usage :
+     * assertAll is use to compare all the value,
+     * evaluate all condition compare to assertEquals
+     *
+     * Comment the @Disabled to review the difference
+     */
+
+    @Test
+    @Disabled
+    @DisplayName("Multiplication Function Test")
+    void mul(){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(4,mathUtils.mul(2,2));
+        assertEquals(2,mathUtils.mul(1,1));
+        assertEquals(3,mathUtils.mul(3,3));
+    }
+
+
+    @Test
+    @Disabled
+    @DisplayName("Assert ALL Multiplication Function Test")
+    void mul1(){
+        MathUtils mathUtils = new MathUtils();
+        assertAll(
+                ()->assertEquals(4,mathUtils.mul(2,2)),
+                ()->assertEquals(2,mathUtils.mul(1,1)),
+                ()->assertEquals(3,mathUtils.mul(3,3))
+        );
+    }
+
+    /**
+     * Test Method 3 : assertThrows
+     *
+     * Usage :
+     * Use to evaluate the Exception
+     * assertThrows(Type of Exception,condition)
+     * when condition is meet the type of Exception,
+     * the test is pass
+     *
+     */
+
+    @Test
+    @DisplayName("Divide Function Test")
+    void divide(){
+        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class,() -> mathUtils.divide(1,0));
+
+    }
+
+    /**
+     * Test Method 4 : ParameterizedTest
+     *
+     * Usage :
+     * Evaluate each element in the ValueSource
+     *
+     */
+
+    @ParameterizedTest
+    @ValueSource(ints ={3,4,5,6})
+    void test(int number){
+        assertEquals(0,number%3);
+
+    }
+
+
+    /**
+     * Test Method 5 : RepeatedTest
+     *
+     * Usage :
+     * Repeat the test base on the number input
+     * RepeatedTest(3) << Repeat the test 3 times
+     *
+     */
+
+    @RepeatedTest(3)
+    @DisplayName("Add Function Test")
+    void repeatAdd() {
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expected, actual, "The sum expected to be 2");
+    }
+
+}
